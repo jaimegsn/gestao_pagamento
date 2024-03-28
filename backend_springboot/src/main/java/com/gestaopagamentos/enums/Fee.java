459c38ed7b2cb149ddc9e4fc5dc3dae8e1fd19b8
unknown <jaime.jgsn2@gmail.com>
2024-03-28 08:56:25 -0300

    Enums

---
+++ backend_springboot/src/main/java/com/gestaopagamentos/enums/Fee.java
@@ -0,0 +1,7 @@
+package com.gestaopagamentos.enums;
+
+public enum Fee {
+    SIMPLE_FEE,
+    COMPOUND_FEE,
+    COSTUMIZED_FEE
+}
---
+++ backend_springboot/src/main/java/com/gestaopagamentos/enums/PaymentMethod.java
@@ -0,0 +1,8 @@
+package com.gestaopagamentos.enums;
+
+public enum PaymentMethod {
+    PIX,
+    CREDIT,
+    DEBIT,
+    MONEY
+}
